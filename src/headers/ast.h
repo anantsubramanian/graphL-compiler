157068ac77247523ace3c0fb5fcda594a275f51f
Aditya Bansal <adityabansal_adi@yahoo.co.in>
2015-03-03 13:07:36 +0530

    Added Header files for Abstract Syntax Tree operations

---
+++ src/headers/ast.h
@@ -0,0 +1,26 @@
+typedef struct ast_node
+{
+  char *name;
+  int num_of_children;
+  struct ast_node **next;
+} ANODE;
+
+typedef struct ast_struct
+{
+  ANODE *root;
+  char *name;
+} AST;
+
+extern AST* getNewAst ();
+
+extern AST* setAstName ( AST * , const char * );
+
+extern ANODE* initializeNode ( ANODE * );
+
+extern ANODE* setNodeName ( ANODE * , const char * );
+
+extern ANODE* setChildren ( ANODE * , int );
+
+extern ANODE* allocateChildren ( ANODE * );
+
+extern ANODE* insertSpaceSeparatedWords ( ANODE * , char * )
